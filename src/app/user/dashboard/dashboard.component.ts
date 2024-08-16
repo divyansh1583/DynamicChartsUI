886bf030681f14dc36fcb12e56659e424a455165
Divyansh Chauhan <divyanshchauhan28@gmail.com>
2024-08-16 13:15:12 +0530

    dashboard updated

--- src/app/user/dashboard/dashboard.component.ts
+++ src/app/user/dashboard/dashboard.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { AudienceMetricsComponent } from "../audience-metrics/audience-metrics.component";
+import { BalanceOverviewComponent } from "../balance-overview/balance-overview.component";
+import { RevenueComponent } from "../revenue/revenue.component";
+import { SalesByLocationsComponent } from "../sales-by-locations/sales-by-locations.component";
+import { SessionsByCountriesComponent } from "../sessions-by-countries/sessions-by-countries.component";
+import { StoreVisitsBySourceComponent } from "../store-visits-by-source/store-visits-by-source.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [AudienceMetricsComponent, BalanceOverviewComponent, RevenueComponent, SalesByLocationsComponent, SessionsByCountriesComponent, StoreVisitsBySourceComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
